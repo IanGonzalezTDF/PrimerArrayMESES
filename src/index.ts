d5a25a7ac8e728fd9e7fdc77891e85bd32528f8d
IanGonzalezTDF <102770923+IanGonzalezTDF@users.noreply.github.com>
2022-06-04 16:48:45 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+let boton = document.getElementById("calcular");
+boton?.addEventListener("click", () => {
+  let arregloMes: string[] = new Array(12);
+  arregloMes[0] = "Enero";
+  arregloMes[1] = "Febrero";
+  arregloMes[2] = "Marzo";
+  arregloMes[3] = "Abril";
+  arregloMes[4] = "Mayo";
+  arregloMes[5] = "Junio";
+  arregloMes[6] = "Julio";
+  arregloMes[7] = "Agosto";
+  arregloMes[8] = "Septiembre";
+  arregloMes[9] = "Octubre";
+  arregloMes[10] = "Noviembre";
+  arregloMes[11] = "Diciembre";
+  let nroMes: number = Number(dato.value);
+  let indice = nroMes - 1;
+  console.log("EL MES ES", arregloMes[indice]);
+});
